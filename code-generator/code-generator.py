49dad9ffc436d85c73866b89c3c469e67f1354ff
Dev Arora <99930525+DevA-rora@users.noreply.github.com>
2023-05-28 06:58:29 +0000

    finished code-generator.py

--- code-generator/code-generator.py
+++ code-generator/code-generator.py
@@ -1,1 +1,8 @@
+# Create a code generator. This can that take text as input, replaces each letter with another letter, and outputs the “encoded” message.
 
+def code_encoder(text):
+    generated_code = text.replace("a", "e").replace("m", "o").replace("x", "y").replace("y", "m").replace("p", "y").replace("r", "0").replace("t", "p").replace("M", "n")
+    return generated_code
+
+code = input("What would you like to encode? ")
+print(code_encoder(code))
